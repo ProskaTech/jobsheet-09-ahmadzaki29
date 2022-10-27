47d35c1a2a685fe860e0a2c90232e340c3af37e2
ahmad zaki alfaruq- 21343079 <112170461+ahmadzaki29@users.noreply.github.com>
2022-10-27 19:41:02 +0700

    Add files via upload

---
+++ js-09-ahmad zaki alfaruq-21343079/latihan1.java
@@ -0,0 +1,19 @@
+package js9;
+
+/*
+ * Created Ahmad Zaki Alfaruq_21343079
+ */
+
+public class latihan1 {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        
+        test(i);
+        System.out.println(i);
+    }
+    
+    public static void test(int j) {
+        j = 33;
+    }
+}
---
+++ js-09-ahmad zaki alfaruq-21343079/latihan3.java
@@ -0,0 +1,23 @@
+package js9;
+
+/*
+ * Created Ahmad Zaki Alfaruq_21343079
+ */
+
+public class latihan3 {
+    public static void main(String[] args) {
+        String str1, str2;
+        str1 = "free the bound periodicals.";
+        str2 = str1;
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same object? " + (str1 == str2));
+        
+        str2 = new String(str1);
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same object? " + (str1 == str2));
+        
+        System.out.println("Same object? " + str1.equals(str2));
+    }
+}
